0711af466d42731b359cb0485b1c3bd69c1a9f3f
Louis Mallez <louis.mallez@epitech.eu>
2018-01-26 23:40:32 +0100

    MousePos

---
+++ Assets/Scripts/MousePos.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MousePos : MonoBehaviour {
+    Vector2Int posOnMap()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        return new Vector2Int((int) (mousePos.x - map.X), (int) (mousePos.y - map.Y));
+    }
+	
+    public Map map;
+}
