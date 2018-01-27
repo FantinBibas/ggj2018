8de2d595dfec97da08343deb2cca837026a4d27d
julian ladjani <julian.ladjani@epitech.eu>
2018-01-27 02:27:31 +0100

    Enemy Controller Find Player

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -4,12 +4,16 @@ using JetBrains.Annotations;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class NaziManager : MonoBehaviour
+public class EnemyController : MonoBehaviour
 {
-    public Map Map;
+    private Map _map;
     public Vector3Int[] RoundPosition;
+    public uint audioRange = 40;
+    public uint viewRange = 40;
+    public float viewAngle = 180;
     public Vector3Int Position = Vector3Int.zero;
     private uint _pointPos;
+    private PlayerController _player;
 
     private enum Direction
     {
@@ -19,31 +23,54 @@ public class NaziManager : MonoBehaviour
 
     private Direction _direction;
 
-    private void Start()
+    private void Awake()
     {
         _direction = Direction.StartToEnd;
+        _map = FindObjectOfType<Map>();
+        _player = (PlayerController) FindObjectOfType(typeof(PlayerController));
     }
 
     public void Play()
     {
         Vector3Int startPos = RoundPosition[_pointPos];
         Vector3Int endPos = GetNextPoint();
-        Path path = Map.NavigateTo(startPos, endPos);
+        Path path = _map.NavigateTo(startPos, endPos);
 
+        viewEnemy();
         if (path == null)
             return;
         foreach (var pathPos in path)
         {
             Position = pathPos;
-            checkEnemy();
+            transform.position = Position;
+            viewEnemy();
         }
+        viewEnemy();
     }
 
-    private bool checkEnemy()
+    private bool viewEnemy()
     {
-        return true;
+        RaycastHit hit;
+        Vector3 rayDirection = _player.transform.position - transform.position;
+
+        if (Vector3.Angle(rayDirection, Vector3.right) <= viewAngle * 0.5f )
+        {
+            Debug.Log("IsIn " + Vector3.Angle(rayDirection, Vector3.right));
+            if (Physics.Raycast(transform.position, rayDirection, out hit, viewRange))
+            {
+                Debug.Log("Player");
+                return hit.transform.CompareTag("player");
+            }
+        }
+
+        return false;
     }
-    
+
+    private void Update()
+    {
+        viewEnemy();
+    }
+
     private Vector3Int GetNextPoint()
     {
         Vector3Int curPointPos = RoundPosition[_pointPos];
