e3de9583b26933e179f177e5aa10fc173ecbde27
Louis Mallez <louis.mallez@epitech.eu>
2018-01-27 09:06:25 +0100

    Un bon début !

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -10,4 +10,5 @@ public class MapGenerator : AMapGenerator
     {
         Grid = grid;
     }
+    
 }
--- Assets/Scripts/Room.cs
+++ Assets/Scripts/Room.cs
@@ -19,4 +19,21 @@ public class Room : MonoBehaviour
         /* Merge content + grid aux coord pos*/
         return true;
     }
+
+    public Vector2Int SizeFromDoor(RoomDoor door, Vector2Int pos)
+    {
+        switch (door.Dir)
+        {
+            case Direction.to.NORTH:
+                return new Vector2Int(pos.x - Pos.x, pos.y);
+            case Direction.to.WEST:
+                return new Vector2Int(pos.x, pos.y - Pos.y); 
+            case Direction.to.SOUTH:
+                return new Vector2Int(pos.x - Pos.x, pos.y - Size.y);
+            case Direction.to.EAST:
+                return new Vector2Int(pos.x - Size.x, pos.y - Pos.y);
+            default:
+                return pos;
+        }
+    }
 }
