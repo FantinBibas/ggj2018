c4fc03e16455f02a8c3cb5bca7bbe694360de8f8
Louis Mallez <louis.mallez@epitech.eu>
2018-01-27 08:28:47 +0100

    Start MapGen

---
+++ Assets/Scripts/Room.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room : MonoBehaviour
+{
+    public Vector2Int Size;
+
+    public RoomDoor NorthDoor;
+    public RoomDoor SouthDoor;
+    public RoomDoor EstDoor;
+    public RoomDoor WestDoor;
+
+    public static bool CheckDoor(RoomDoor Door)
+    {
+        return Door && Door.isValid();
+    }
+}
---
+++ Assets/Scripts/RoomDoor.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomDoor : MonoBehaviour {   
+    public int Pos;
+
+    public bool isValid()
+    {
+        return Pos > 0;
+    }
+}
