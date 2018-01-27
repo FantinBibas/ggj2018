910f00c11075a42bc20aa5f58ceff45b68751632
Yannick Suc <suc.yannick@gmail.com>
2018-01-27 20:10:55 +0100

    [Textures / Menu] Add menu and textures

---
+++ Assets/Scripts/menuScript.cs
@@ -0,0 +1,47 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;// we need this namespace in order to access UI elements within our script
+using System.Collections;
+using UnityEngine.SceneManagement; // neded in order to load scenes
+ 
+public class menuScript : MonoBehaviour
+{
+    public Canvas quitMenu;
+    public Button startText;
+    public Button exitText;
+
+    void Start()
+    {
+        quitMenu = quitMenu.GetComponent<Canvas>();
+        startText = startText.GetComponent<Button>();
+        exitText = exitText.GetComponent<Button>();
+        quitMenu.enabled = false;
+
+    }
+
+    public void ExitPress()
+    {
+        quitMenu.enabled = true;
+        startText.enabled = false;
+        exitText.enabled = false;
+    }
+
+    public void NoPress()
+    {
+        quitMenu.enabled = false; //we'll disable the quit menu, meaning it won't be visible anymore
+        startText.enabled = true; //enable the Play and Exit buttons again so they can be clicked
+        exitText.enabled = true;
+    }
+
+    public void StartLevel()
+    {
+        Debug.Log("yo\n");
+        SceneManager.LoadScene("Scenes/MiniHackingGame");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+}
